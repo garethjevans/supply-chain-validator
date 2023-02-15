5331d8ac0c08f39b666f8a55c52dc0496e692a7d
Gareth Evans <gareth@bryncynfelin.co.uk>
2023-02-15 13:07:57 +0000

    chore: adding debug scripts

---
+++ debug.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -euo pipefail
+
+tanzu apps workload get library-supply-chain
---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+make build
+echo "================================================="
+
+echo "Validate"
+ytt -f ./config | ./build/scv validate -
+echo "================================================="
+
+echo "Diff"
+ytt -f ./config | kubectl diff -f -
+echo "================================================="
